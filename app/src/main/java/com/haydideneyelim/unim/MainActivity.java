59c9e1937028392d5bf70de8dc6d20ce7417e795
Muhammet Çeneli <muhammetceneli@gmail.com>
2020-02-25 21:16:32 +0300

    ana ekran tasarımı material yapıldı, butonlar yerine pop-up menü eklendi.

--- app/src/main/java/com/haydideneyelim/unim/MainActivity.java
+++ app/src/main/java/com/haydideneyelim/unim/MainActivity.java
@@ -2,11 +2,12 @@ package com.haydideneyelim.unim;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,9 +25,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView welcomeText = findViewById(R.id.welcomeText);
-        Button kullaniciSil = findViewById(R.id.kullaniciSil);
-        Button cikisYap = findViewById(R.id.cikisYap);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        TextView usernameText = findViewById(R.id.Username);
 
         auth = FirebaseAuth.getInstance();
 
@@ -43,38 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        welcomeText.setText("Merhaba, " + user.getEmail());
-
-        kullaniciSil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(user != null){
-                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this, "Hesabın silindi.Yeni bir hesap oluştur!", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(MainActivity.this, UyeOlActivity.class));
-                                finish();
-                            } else {
-                                Toast.makeText(MainActivity.this, "Hesap silinemedi!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-
-            }
-        });
-
-        cikisYap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                auth.signOut();
-                startActivity(new Intent(MainActivity.this, GirisYapActivity.class));
-                finish();
-            }
-        });
+        usernameText.setText(user.getEmail());
 
     }
 
@@ -92,4 +64,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.cikisYap:
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this, GirisYapActivity.class));
+                finish();
+                return true;
+
+            case R.id.kullaniciSil:
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user != null){
+                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Hesabın silindi.Yeni bir hesap oluştur!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this, UyeOlActivity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Hesap silinemedi!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+        }
+
+        return false;
+
+    }
 }
